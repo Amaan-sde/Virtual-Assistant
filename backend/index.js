21f4b9cbd1d3d2e10513b1158fbfda7bbf85ec0f
Amaan-sde <amaankhan172595@gmail.com>
2025-12-07 06:29:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://virtual-assistant-gmxi.onrender.com',
     credentials: true
 }));
 
